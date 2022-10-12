bc19139632086ea473ef1d498fd093873082adb9
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-10-12 20:58:26 +0600

    [statistics create]

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,8 +2,93 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div>
-      <h2>Blog</h2>
+    <div className="mt-[80px]">
+      <h1 className="mb-10 text-2xl font-semibold flex justify-center p-5 bg-white rounded-lg">
+        Question & Ans
+      </h1>
+      <ol className="relative border-l border-gray-200 dark:border-gray-700 lg:mx-[150px] mx-[20px]">
+        <li className="mb-10 ml-6">
+          <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg
+              aria-hidden="true"
+              className="w-3 h-3 text-blue-600 dark:text-blue-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </span>
+          <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            How does React Work ?
+          </h3>
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            ReactJS divides the UI into isolated reusable pieces of code known
+            as components. React components work similarly to JavaScript
+            functions as they accept arbitrary inputs called properties or
+            props. It's possible to have as many components as necessary without
+            cluttering your code.
+          </p>
+        </li>
+        <li className="mb-10 ml-6">
+          <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg
+              aria-hidden="true"
+              className="w-3 h-3 text-blue-600 dark:text-blue-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </span>
+          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            Difference between state and props in react ?
+          </h3>
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Props are used to pass data from one component to another. The state
+            is a local data storage that is local to the component only and
+            cannot be passed to other components.
+          </p>
+        </li>
+        <li className="ml-6">
+          <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg
+              aria-hidden="true"
+              className="w-3 h-3 text-blue-600 dark:text-blue-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </span>
+          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            What does useEffect do ?
+          </h3>
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            If we perform a side effect directly in our component body, it gets
+            in the way of our React component's rendering. Side effects should
+            be separated from the rendering process. If we need to perform a
+            side effect, it should strictly be done after our component renders.
+            This is what useEffect gives us .In short, useEffect is a tool that
+            lets us interact with the outside world but not affect the rendering
+            or performance of the component that it's in.
+          </p>
+        </li>
+      </ol>
     </div>
   );
 };
